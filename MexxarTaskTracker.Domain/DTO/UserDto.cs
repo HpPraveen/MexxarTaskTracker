40c3c1a2cdda23a6b72035d4665b0e73a8abd67b
Praveen Howpage <howpagepraveen@gmail.com>
2023-08-24 19:43:56 +0530

    .

--- MexxarTaskTracker.Domain/DTO/UserDto.cs
+++ MexxarTaskTracker.Domain/DTO/UserDto.cs
@@ -4,7 +4,6 @@ namespace MexxarTaskTracker.Domain
 {
     public class UserDto
     {
-        public long Id { get; set; }
         public string? UserId { get; set; }
         public string? UserName { get; set; }
         public string? FirstName { get; set; }
